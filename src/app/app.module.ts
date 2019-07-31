2097257d4693cd86d1c5428340d15fc8314e93de
Peter <katalol95@gmail.com>
2019-07-31 20:51:29 +0300

    just create app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrendingComponent } from './trending/trending.component';
+import { DetailComponent } from './detail/detail.component';
+import { SavedComponent } from './saved/saved.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PagesComponent } from './pages/pages.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrendingComponent,
+    DetailComponent,
+    SavedComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+const MaterialComponents = [
+
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
