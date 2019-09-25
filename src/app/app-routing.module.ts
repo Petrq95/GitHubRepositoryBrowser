10f42bf710543f4517bfbcd8cd29854290e5b1d4
ptrovi4 <katalol95@gmail.com>
2019-09-25 10:30:28 +0300

    Scroll restore module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router, Scroll,} from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RetainScrollPolyfillModule } from './retain-scroll-polyfill/retain-scroll-polyfill.module';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/pages', pathMatch: 'full' },
   { path: 'pages', loadChildren: () => import('./explorer/explorer.module').then(m => m.ExplorerModule), },
-/*   { path: '404', component: NotFoundComponent },
-  { path: '**', redirectTo: '/404' }, */
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  RetainScrollPolyfillModule.forRoot({
+    // Tell the polyfill how long to poll the document after a route change in
+    // order to look for elements that need to be restored to a previous offset.
+    pollDuration: 3000,
+    pollCadence: 50
+  }),
+],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
